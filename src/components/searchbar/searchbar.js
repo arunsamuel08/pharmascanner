968708336c7e0884aec5879c6e1abea9c0d8c871
Arun Saji Samuel <arun.samuel08@gmail.com>
2024-08-31 12:31:03 +0530

    Format Changes

--- src/components/searchbar/searchbar.js
+++ src/components/searchbar/searchbar.js
@@ -3,7 +3,7 @@ import "./searchbar.css";
 const Searchbar = () => {
   return (
     <div className="search-cont">
-      <input className="input-box" placeholder="Azithromycin 500mg" />
+      <input className="input-box" placeholder="Search for 'Azithromycin 500mg'" />
       <button className="search-btn">
         <img src="./images/search.png" alt="search" />
       </button>

d385ff9f6984cb23c545d0d319e23d56ace1b208
Arun Saji Samuel <arun.samuel08@gmail.com>
2024-08-31 17:03:16 +0530

    added suggestion box

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/navbar/navbar";
 import Searchbar from "./components/searchbar/searchbar";
 
 function App() {
+  const suggestionList = [
+    "Azithromycin 500mg",
+    "Azithromycin 250mg",
+    "Paracetamol 500mg",
+    "Paracetamol 1000mg",
+    "Clarithromycin 500mg",
+    "Atorvastatin 50mg",
+    "Acetaminophen",
+    "Adderall",
+    "Amitriptyline",
+    "Amlodipine",
+    "Amoxicillin",
+    "Ativan"
+  ];
+
+  const [searchTerm, setsearchTerm] = useState("");
+  let filteredData = suggestionList;
+  if (searchTerm) {
+    filteredData = filteredData.filter(i =>
+      i.toLowerCase().startsWith(searchTerm.toLowerCase())
+    );
+  } else if (searchTerm === "") {
+    filteredData = [];
+  }
+
   return (
     <div className="App">
       <div className="home-searchcontainer">
@@ -10,8 +36,13 @@ function App() {
         <div className="taglinecont">
           <h4>Unlock the Lowest Prices on Medicines, Every Time.</h4>
         </div>
-        <Searchbar />
+        <Searchbar
+          searchTerm={searchTerm}
+          setsearchTerm={setsearchTerm}
+          filteredData={filteredData}
+        />
       </div>
+      <div className="card-container">Hello</div>
     </div>
   );
 }
--- src/components/searchbar/searchbar.js
+++ src/components/searchbar/searchbar.js
@@ -1,12 +1,36 @@
 import "./searchbar.css";
 
-const Searchbar = () => {
+const Searchbar = ({ searchTerm, setsearchTerm, filteredData }) => {
+  const handleinputChange = e => {
+    setsearchTerm(e.target.value);
+  };
+
   return (
-    <div className="search-cont">
-      <input className="input-box" placeholder="Search for 'Azithromycin 500mg'" />
-      <button className="search-btn">
-        <img src="./images/search.png" alt="search" />
-      </button>
+    <div className="searchbox-cont">
+      <div className="search-cont">
+        <input
+          className="input-box"
+          placeholder="Search for 'Azithromycin 500mg'"
+          value={searchTerm}
+          onChange={handleinputChange}
+        />
+        <button className="search-btn">
+          <img src="./images/search.png" alt="search" />
+        </button>
+      </div>
+      <div className="suggestion-box">
+        {filteredData.length > 0 && (
+          <ul className="suggestions-list">
+            {filteredData.map((suggestion, index) => (
+              <a href="#home">
+                <li key={index} className="suggestion-item">
+                  {suggestion}
+                </li>
+              </a>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 };
